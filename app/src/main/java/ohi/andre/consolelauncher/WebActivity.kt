8d9dcf6e6a64f4207f41b9a12f7eeb5d686866fe
John Nduli <rookie101@jnduli.co.ke>
2026-04-26 14:15:44 +0100

    diable edge_to_edge after adding app bar for reloading

--- app/src/main/java/ohi/andre/consolelauncher/WebActivity.kt
+++ app/src/main/java/ohi/andre/consolelauncher/WebActivity.kt
@@ -6,10 +6,7 @@ import android.view.MenuItem
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.activity.OnBackPressedCallback
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class WebActivity : AppCompatActivity() {
     companion object {
@@ -21,13 +18,7 @@ class WebActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_web)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
         myWebView = findViewById<WebView>(R.id.webView)
         myWebView.settings.javaScriptEnabled = true
         myWebView.settings.domStorageEnabled = true
